e51a5d8ccc5ce38da665cdd6699d5adc8d7058e9
leticia <leticiaavolio09@gmail.com>
2025-03-17 22:23:01 -0300

    Commit Inicial

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Switch, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Header</Text>
+      </View>
+      <View style={styles.body}>
+      <Text>Hello, World!</Text>
+      <Text>Já já, Lanchão 🌭🍔🥪🥟🥡🥧</Text>
+      <Text>Ligar</Text>
+      <Switch/>
+      </View>
     </View>
   );
 }
@@ -17,4 +25,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  body:{
+    flex:3,
+    justifyContent: 'center',
+  },
+  header:{
+    width: '100%',
+    flex:1,
+    backgroundColor:'#085A8C',
+    justifyContent:'center',
+    alignItems:'center',
+    paddingTop:22,
+  },
+  headerText:{
+    color:'#fff',
+    fontSize:30,
+  }
 });
