730c61394fb345ce9e1d0464c8a7abef3c35989b
leticia <leticiaavolio09@gmail.com>
2025-04-22 22:17:36 -0300

    Tornei o titulo e a descrição de cada post diferentes

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,11 +1,17 @@
 import { StyleSheet, Text, View, Image} from 'react-native';
 import Button from '../Button/Button';
+import React from 'react';
 
-export default function Post() {
+type Prop ={
+  titulo:string,
+  descricao:string,
+}
+
+export default function Post(prop: Prop) {
   return (
     <View style={styles.post}>
-        <Text style={styles.headerPost}>Rio de Janeiro</Text>
-        <Text style={styles.textPost}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aliquam, odit consectetur dolore tempore autem eaque reprehenderit voluptatum fuga.</Text>
+        <Text style={styles.headerPost}>{prop.titulo}</Text>
+        <Text style={styles.textPost}>{prop.descricao}</Text>
         <Image source={require('../../assets/rj.jpg')} style={styles.image} />
         <Button />
     </View>
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <View style={styles.container}>
         <StatusBar style="auto" />
         <Header/>
-        <Post />
-        <Post />
+        <Post titulo="Rio de Janeiro" descricao="O Rio de Janeiro é uma grande cidade brasileira à beira-mar, famosa pelas praias de Copacabana e Ipanema, pela estátua do Cristo Redentor."/>
+        <Post titulo="São Paulo" descricao="São Paulo, está entre as cidades mais populosas do mundo, com diversas instituições culturais e uma rica tradição arquitetônica."/>
         <Footer />
       </View>
     );
